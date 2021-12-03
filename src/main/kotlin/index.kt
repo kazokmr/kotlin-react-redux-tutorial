7e3d4080a42d16e16996a47d04d795a9d5f645fe
Kazuhiko Okumura <engkokmr@gmail.com>
2021-12-04 08:10:27 +0900

    Add Style sheet
    index.cssを作成して取り込むようにした

--- src/main/kotlin/index.kt
+++ src/main/kotlin/index.kt
@@ -1,6 +1,8 @@
+import kotlinext.js.require
 import kotlinx.browser.document
 import react.dom.render
 
 fun main() {
+    require("./index.css")
     document.getElementById("root")?.let { render(it) { child(app) } }
 }
