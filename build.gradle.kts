f2f8030cb6b4f7b73771f9d06e9929da009c0e81
Kazuhiko Okumura <engkokmr@gmail.com>
2021-12-15 06:07:34 +0900

    Kotlinのバージョンを1.6.10にアップデート
    Kotlin wrappersの依存関係のバージョン管理をkotlin-wrappers-bomに委譲させた
    - Kotlin Wrappersで利用したい依存関係の指定を簡略に行えるような関数を追加した

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("js") version "1.6.0"
+    kotlin("js") version "1.6.10"
 }
 
 group = "me.kazokmr"
@@ -9,13 +9,21 @@ repositories {
     mavenCentral()
 }
 
+// kotlin-wrappersのGroupIdとArtifactIdの共通文字列テンプレート
+fun kotlinw(target: String): String =
+    "org.jetbrains.kotlin-wrappers:kotlin-$target"
+
+// kotlin-wrappers-bom (Kotlin Wrappersの依存関係バージョンの管理をしてくれるBOM)のバージョンを指定する
+val kotlinWrappersVersion = "0.0.1-pre.280-kotlin-1.6.0"
+
 dependencies {
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.274-kotlin-1.6.0")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.274-kotlin-1.6.0")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-redux:7.2.6-pre.274-kotlin-1.6.0")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-router-dom:6.0.2-pre.274-kotlin-1.6.0")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-redux:4.1.2-pre.274-kotlin-1.6.0")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-styled:5.3.3-pre.274-kotlin-1.6.0")
+    implementation(enforcedPlatform(kotlinw("wrappers-bom:$kotlinWrappersVersion")))
+    implementation(kotlinw("react"))
+    implementation(kotlinw("react-dom"))
+    implementation(kotlinw("react-redux"))
+    implementation(kotlinw("react-router-dom"))
+    implementation(kotlinw("redux"))
+    implementation(kotlinw("styled"))
     implementation(npm("file-loader", "^6.2.0"))
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.6.0")
 }
